83a607afcf113ff50ae90a00217f02bcfea17f35
aten88 <virtuoso_ten@mail.ru>
2023-12-29 17:21:32 +0300

    Создал класс формы и доб субьекты в yacut.html

--- yacut_app.py
+++ yacut_app.py
@@ -2,12 +2,16 @@ from datetime import datetime
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'H4#dl2Fm7'
 
 db = SQLAlchemy(app)
 
@@ -20,10 +24,22 @@ class URLMap(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
+class URLForm(FlaskForm):
+    original_link = TextAreaField(
+        'Укажите полную ссылку',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    custom_id = StringField(
+        'Укажите короткую ссылку',
+        validators=[Length(1, 6)]
+    )
+    submit = SubmitField('Создать')
+
+
 @app.route('/')
 def my_index_view():
-    # urlmap = URLMap.query.get(1)
-    return render_template('yacut.html')
+    form = URLForm()
+    return render_template('yacut.html', form=form)
 
 
 if __name__ == '__main__':
