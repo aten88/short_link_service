a9a69510ef3aed185b4f8380ce0752cdc293d045
aten88 <virtuoso_ten@mail.ru>
2024-01-05 12:24:18 +0300

    Сделал добавление данных через эндпоинт

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,10 +1,22 @@
-from flask import jsonify
+from flask import jsonify, request
 
-from . import app
+from . import app, db
 from .models import URLMap
+
+
+@app.route('/api/id/', methods=['POST'])
+def create_id():
+    """ Метод создания записи через API. """
+    data = request.get_json()
+    url_map = URLMap()
+    url_map.from_dict(data)
+    db.session.add(url_map)
+    db.session.commit()
+    return jsonify({'url': url_map.url_to_dict()}), 201
 
 
 @app.route('/api/id/<short_id>/', methods=['GET'])
 def get_url(short_id):
+    """ Метод получения ссылки по идентификатору. """
     url_map = URLMap.query.filter_by(short=short_id).first_or_404()
-    return jsonify({'url': url_map.original}), 200
+    return jsonify({'data': url_map.original}), 200
--- yacut/models.py
+++ yacut/models.py
@@ -9,3 +9,18 @@ class URLMap(db.Model):
     original = db.Column(db.Text, unique=True, nullable=False)
     short = db.Column(db.String(16), unique=True, nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def url_to_dict(url_map):
+        """ Метод преобразования данных в словарь. """
+        return dict(
+            id=url_map.id,
+            original=url_map.original,
+            short=url_map.short,
+            timestamp=url_map.timestamp
+        )
+
+    def from_dict(self, data):
+        """Метод преобразования данных из словаря. """
+        for field in ['original', 'short', 'timestamp']:
+            if field in data:
+                setattr(self, field, data[field])
