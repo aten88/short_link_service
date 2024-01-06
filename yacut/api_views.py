921912363ccd9d8f52e731ecf9844ddf6759d83a
aten88 <virtuoso_ten@mail.ru>
2024-01-06 17:30:42 +0300

    3 теста прошел осталось еще 2

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,5 +1,5 @@
 from flask import jsonify, request
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import SQLAlchemyError
 
 from . import app, db
 from .models import URLMap
@@ -10,24 +10,29 @@ from .utils import create_random_url
 def create_id():
     """ Метод создания записи через API. """
     data = request.get_json()
+    if not data:
+        return jsonify({'message': 'Отсутствует тело запроса'}), 400
     original_url = data.get('url')
     short_url = data.get('custom_id')
-    if short_url is None:
+    if short_url is None or not short_url:
         short_url = create_random_url()
     if len(short_url) > 16:
         return jsonify({'message': 'Указано недопустимое имя для короткой ссылки'}), 400
     existing_short_url = URLMap.query.filter_by(short=short_url).first()
     if existing_short_url:
         return jsonify({'message': 'Предложенный вариант короткой ссылки уже существует.'}), 400
+    existing_url = URLMap.query.filter_by(original=original_url).first()
+    if existing_url:
+        return jsonify({'message': 'Предложенный вариант полной ссылки уже существует.'}), 400
     if not (short_url.isalnum() and short_url.isascii()):
         return jsonify({'message': 'Указано недопустимое имя для короткой ссылки'}), 400
     url_map = URLMap(original=original_url, short=short_url)
     try:
         db.session.add(url_map)
         db.session.commit()
-    except IntegrityError:
+    except SQLAlchemyError:
         db.session.rollback()
-        return jsonify({'error': 'Отсутствует тело запроса'}), 400
+        return jsonify({'message': '\"url\" является обязательным полем!'}), 400
     return jsonify(url_map.url_to_dict()), 201
 
 
